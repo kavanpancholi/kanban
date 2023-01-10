dc9ba124842a88d3b89d7b33fe00a47b51d0622f
Kavan Pancholi <kavanpancholi@gmail.com>
2023-01-10 10:18:52 +0530

    Setup vue with vite

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,22 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue'
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
-});
+export default ({ mode }) => {
+    process.env = Object.assign(process.env, loadEnv(mode, process.cwd(), ''));
+
+    return defineConfig({
+        server: {
+            hmr: {
+                host: process.env.VITE_APP_URL,
+            },
+        },
+        plugins: [
+            vue(),
+            laravel({
+                input: ['resources/css/app.css', 'resources/js/app.js'],
+                refresh: true,
+            }),
+        ],
+    })
+};
